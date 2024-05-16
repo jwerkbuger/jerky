7e9cd4dd9a8f93bf3cadcd471242d0fa7369a322
jwerkbuger <jwerkbuger@gmail.com>
2024-05-16 19:28:36 +0900

    Gugudan-Assignment

--- python/Gugudan-assignment/gugudan_assignment2.py
+++ python/Gugudan-assignment/gugudan_assignment2.py
@@ -11,7 +11,13 @@ def main():
     row = int(input("한 행에 출력할 구구단의 개수를 입력하세요: "))
     dan = int(input("최대 출력할 단 수를 입력하세요: "))
 
-    for startdan in range(1, dan + 1, row):
-        gugudan(startdan, dan, row)
+    if row > 0:
+        if dan > 0:
+            for startdan in range(1, dan + 1, row):
+                gugudan(startdan, dan, row)
+        else:
+            print("한 행에 출력할 구구단의 개수가 1보다 작아서 실행이 불가능합니다.")
+    else:
+        print("출력할 행의 개수가 1보다 작아서 실행이 불가능합니다.")
 
 main()
